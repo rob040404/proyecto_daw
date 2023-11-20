0ea42fb30229f81c53e7a8aa7bc20d1495cdf8b1
FoxArtM <atwmss28@gmail.com>
2023-11-20 20:28:10 +0100

    Constructor empleado.php

--- src/modelo/Empleado.php
+++ src/modelo/Empleado.php
@@ -5,10 +5,8 @@ namespace App\modelo;
 // PDO se usa para interaccionar con la base de datos relacional
 use \PDO as PDO;
 
-/**
- * Usuario representa al usuario que está usando la aplicación
- */
-class Empleado {
+class Empleado
+{
 
     private $id_usuario;
     private $nombre;
@@ -17,33 +15,33 @@ class Empleado {
     private $rol;
     private $email;
 
-    //CAMBIAR NOMBRES DE PARAMETROS con comas sin tipo de dato y sn =null
-  public function __construct(string $nombre = null, string $clave = null, string $email = null) {
+    public function __construct($id_usuario, $nombre, $apellidos, $contrasena, $rol, $email)
+    {
+        if (!is_null($id_usuario)) {
+            $this->id_usuario = $id_usuario;
+        }
         if (!is_null($nombre)) {
             $this->nombre = $nombre;
         }
-        if (!is_null($clave)) {
-            $this->clave = $clave;
+        if (!is_null($apellidos)) {
+            $this->apellidos = $apellidos;
+        }
+        if (!is_null($contrasena)) {
+            $this->contrasena = $contrasena;
+        }
+        if (!is_null($rol)) {
+            $this->rol = $rol;
         }
         if (!is_null($email)) {
             $this->email = $email;
         }
     }
 
-
-    /**
-     * Get the value of id_usuario
-     */ 
     public function getId_usuario()
     {
         return $this->id_usuario;
     }
 
-    /**
-     * Set the value of id_usuario
-     *
-     * @return  self
-     */ 
     public function setId_usuario($id_usuario)
     {
         $this->id_usuario = $id_usuario;
@@ -51,19 +49,11 @@ class Empleado {
         return $this;
     }
 
-    /**
-     * Get the value of nombre
-     */ 
     public function getNombre()
     {
         return $this->nombre;
     }
 
-    /**
-     * Set the value of nombre
-     *
-     * @return  self
-     */ 
     public function setNombre($nombre)
     {
         $this->nombre = $nombre;
@@ -71,19 +61,11 @@ class Empleado {
         return $this;
     }
 
-    /**
-     * Get the value of apellidos
-     */ 
     public function getApellidos()
     {
         return $this->apellidos;
     }
 
-    /**
-     * Set the value of apellidos
-     *
-     * @return  self
-     */ 
     public function setApellidos($apellidos)
     {
         $this->apellidos = $apellidos;
@@ -91,19 +73,11 @@ class Empleado {
         return $this;
     }
 
-    /**
-     * Get the value of contrasena
-     */ 
     public function getContrasena()
     {
         return $this->contrasena;
     }
 
-    /**
-     * Set the value of contrasena
-     *
-     * @return  self
-     */ 
     public function setContrasena($contrasena)
     {
         $this->contrasena = $contrasena;
@@ -111,19 +85,11 @@ class Empleado {
         return $this;
     }
 
-    /**
-     * Get the value of rol
-     */ 
     public function getRol()
     {
         return $this->rol;
     }
 
-    /**
-     * Set the value of rol
-     *
-     * @return  self
-     */ 
     public function setRol($rol)
     {
         $this->rol = $rol;
@@ -131,19 +97,11 @@ class Empleado {
         return $this;
     }
 
-    /**
-     * Get the value of email
-     */ 
     public function getEmail()
     {
         return $this->email;
     }
 
-    /**
-     * Set the value of email
-     *
-     * @return  self
-     */ 
     public function setEmail($email)
     {
         $this->email = $email;
