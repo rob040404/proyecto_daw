6d517cb748b00b7495c59ebdf14f849a0cd84f8e
Marcos Espinosa Luengo <marcosespi125@gmail.com>
2023-11-21 18:30:47 +0100

    Implementamos las paginas nueva reserva y borrar reserva

--- views/nueva_reserva.blade.php
+++ views/nueva_reserva.blade.php
@@ -1,22 +1,33 @@
 @extends('app')
-@section('titulo', 'Reservas')
+@section('titulo', 'Nueva Reserva')
 @section('content')
 <div class="reservas-container">
     <img class="background-icon" alt="" src="../public/assets/img/background@2x.png"/>
     <div class="reservas-contenido">
-        <a class="boton-volver" href="login.php"><- Volver</a>
+        <a class="boton-volver" href="reservas.php"><- Volver</a>
         <h1 class="titulo-reservas">Reservas</h1>
-        <div class="botones-reserva">
-          <div class="boton-nueva-reserva">
-              <div><a href="../public/nueva_reserva.php">Nueva Reserva</a></div>
-          </div>
-          <div class="boton-modificar">
-            <div><a href="#">Modificar Reserva</a></div>
-          </div>
-          <div class="boton-eliminar">
-            <div><a href="../public/eliminar_reservas.php">Eliminar reserva/s</a></div>
-          </div>
-        </div>
+        <h1 class="titulo-nueva-reserva">Nueva Reserva</h1>
+        <form method="post" action="#">
+            <div class="nueva-reserva-container">
+                <div class="form-containers">
+                    <div class="form-container1">
+                        <label>Nombre:</label>
+                        <input type="text">
+                        <label class="titulo-form">Telefono:</label>
+                        <input type="tel">
+                        <label class="titulo-form">Nº Comensales:</label>
+                        <input type="number">
+                    </div>
+                    <div class="form-container2">
+                        <label>Fecha:</label>
+                        <input type="text">
+                        <label class="titulo-form">Correo electronico:</label>
+                        <input type="email">
+                        <input class="boton-guardar" type="submit" value="Confirmar la reserva">
+                    </div>
+                </div>
+            </div>
+        </form>
         <h1 class="listado-de-reservas">Listado de reservas</h1>
         <div class="contenedor-tabla-listado-de-reservas">
             <form method="post">
