b24896b1bb1184659047ede194e6c22b2449a83a
rob040404 <robert.gr704@gmail.com>
2023-12-12 16:24:08 +0100

    Clases Plato y PlatoDAO, y tabla con platos en data.sql

--- src/BD/data.sql
+++ src/BD/data.sql
@@ -20,3 +20,14 @@ VALUES
 ('Lechuga', '0.50', '20'),
 ('Pollo', '6', '9.5'),
 ('Tortita', '1', '100');
+
+
+INSERT INTO `platos` (`nombre`, `ingredientes`, `categoria`, `subcategoria`, `precio`, `estado`) VALUES
+('tacos al pastor', 'Carne a la brasa, cilantro, lima, tortita de maiz', 'principal', 'tacos', '8.90', 'activado'),
+('nachos con guacamole', 'Nachos de maíz, con guacamole, salsa agria, pico de gallo, jalapeños', 'entrante', 'nachos', '12.80', 'activado'),
+('nachos con queso', 'Nachos con queso fundido, frijoles, ternera', 'entrante', 'nachos', '11.80', 'activado'),
+('tacos cochinita pibil', 'Tacos con cochinita pibil y un poco de cebolla roja (4uds.)', 'principal', 'tacos', '12.40', 'activado'),
+('ensalada de pollo', 'Pollo, tiras de maiz, lechuga, col lombarda, manzana y cebolla agridulce', 'principal', 'ensalada', '10.90', 'activado'),
+('sorbete de limón y tequila', 'Delicioso sorbete con limón y tequila', 'postre', NULL, '6.60', 'activado'),
+('corona', NULL, 'bebida', 'alcoholicas', '3.95', 'activado'),
+('coca-cola', NULL, 'bebida', 'refresco', '3.60', 'activado');
